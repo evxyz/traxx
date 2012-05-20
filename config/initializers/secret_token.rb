60a3531cbf523efaf98e1f14e8a7abd14cc5c333
evx001 <eviexeris@yahoo.com>
2012-05-20 08:24:58 -0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traxx::Application.config.secret_token = '818fd820c569481f00fb9a292deeca935ece717606f54adab4c4e1fdff97d9b454a890faa5ee65bd120014279fec8266a47504b582e6f55677b98f456fa03f70'
